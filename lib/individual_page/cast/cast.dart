b527058357e3213fb98897cee7dc244d6d79fbf7
jungee <jungebah@gmail.com>
2020-08-06 23:33:30 -0700

    added space on top

--- lib/individual_page/cast/cast.dart
+++ lib/individual_page/cast/cast.dart
@@ -15,102 +15,117 @@ class CastPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 500.0,
-            floating: true,
-            pinned: true,
-            snap: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(30),
+      body: SafeArea(
+        child: new CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 500.0,
+              floating: true,
+              pinned: true,
+              snap: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              actionsIconTheme: IconThemeData(opacity: 0.0),
+              leading: IconButton(
+                icon: FaIcon(
+                  FontAwesomeIcons.arrowCircleLeft,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              flexibleSpace: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              spreadRadius: 6,
+                              blurRadius: 4,
+                              offset:
+                                  Offset(0, 4), // changes position of shadow
+                            ),
+                          ],
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(actor['image'])),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30),
+                          )),
+                    ),
+                  )
+                ],
               ),
             ),
-            actionsIconTheme: IconThemeData(opacity: 0.0),
-            leading: IconButton(
-              icon: FaIcon(
-                FontAwesomeIcons.arrowCircleLeft,
-                size: 30,
-                color: Colors.white,
-              ),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            flexibleSpace: Stack(
-              children: <Widget>[
-                Positioned.fill(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          fit: BoxFit.cover, image: AssetImage(actor['image'])),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  new SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: new Text(
+                      actor['name'],
+                      style: GoogleFonts.merriweather(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                      ),
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                new SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: new Text(
-                    actor['name'],
-                    style: GoogleFonts.merriweather(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic,
+                  new SizedBox(height: 20),
+                  new Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 5),
+                    child: new Text(
+                      'Movies',
+                      style: GoogleFonts.merriweather(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                      ),
                     ),
                   ),
-                ),
-                new SizedBox(height: 15),
-                new Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: new Text(
-                    'Movies',
-                    style: GoogleFonts.merriweather(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                new SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        ...cast.map(
-                          (e) => Row(
-                            children: <Widget>[
-                              SizedBox(
-                                height: 150.0,
-                                width: 120.0,
-                                child: new Card(
-                                  semanticContainer: true,
-                                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                                  child: new Image.asset(e, fit: BoxFit.fill),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
+                  new SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ...cast.map(
+                            (e) => Row(
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 150.0,
+                                  width: 120.0,
+                                  child: new Card(
+                                    semanticContainer: true,
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    child: new Image.asset(e, fit: BoxFit.fill),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    elevation: 5,
+                                    margin: EdgeInsets.all(4),
                                   ),
-                                  elevation: 5,
-                                  margin: EdgeInsets.all(4),
                                 ),
-                              ),
-                              new SizedBox(width: 8)
-                            ],
-                          ),
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          )
-        ],
+                                new SizedBox(width: 8)
+                              ],
+                            ),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
