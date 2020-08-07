51b1af74e1374e46aae0f44274c16475246d471b
jungee <jungebah@gmail.com>
2020-08-06 22:32:10 -0700

    change artist page

--- lib/individual_page/cast/cast.dart
+++ lib/individual_page/cast/cast.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CastPage extends StatelessWidget {
-  final cast = [
+  static final cast = [
     'assets/Loot.jpg',
     'assets/whiteSun.jpg',
     'assets/chadke.jpg',
@@ -14,31 +15,44 @@ class CastPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: new CustomScrollView(
         slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 500.0,
+            floating: true,
+            pinned: true,
+            snap: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(30),
+              ),
+            ),
+            actionsIconTheme: IconThemeData(opacity: 0.0),
+            leading: IconButton(
+              icon: FaIcon(
+                FontAwesomeIcons.arrowCircleLeft,
+                size: 30,
+                color: Colors.white,
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            flexibleSpace: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover, image: AssetImage(actor['image'])),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                new SizedBox(
-                  height: 400,
-                  child: Card(
-                    semanticContainer: true,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    child: Hero(
-                      tag: actor['image'],
-                      child: new Image.asset(
-                        this.actor['image'],
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    elevation: 5,
-                    margin: EdgeInsets.all(4),
-                  ),
-                ),
                 new SizedBox(
                   height: 10,
                 ),
